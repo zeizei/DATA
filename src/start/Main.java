a615eb33040285574781d613f7b390e076cb40e1
B_O_B <1248373163@qq.com>
2015-06-05 09:41:04 +0800

    完成数据采集项目

--- src/start/Main.java
+++ src/start/Main.java
@@ -3,11 +3,11 @@ package start;
 public class Main {
 
 	public static void main(String[] args) {
-		Task task = new Task();
+		// Task task = new Task();
 		// task.createDB();
 		// task.getMatchs();
 		// task.getPlayer();
-		task.getTeam();
+		// task.getTeam();
 		// task.getPlayOff();
 		// task.getPlayerImage();
 	}
